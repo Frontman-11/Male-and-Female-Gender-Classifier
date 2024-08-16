211834e141589cea3e95303a8f85487cf2c77cc4
Frontman <isong.nathan90@gmail.com>
2024-08-16 03:29:53 +0100

    Updated docstrings.

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 def train_model(model,
                 X_train,
                 valid_set,
@@ -11,26 +12,24 @@ def train_model(model,
                 insert_dropout=None,
                 save_model_to=None,
                 show_model_summary=False,
-                **kwargs
-               ):
+                **kwargs):
     """
-    Trains the model with optional loading and saving of weights, dynamic adjustment of the model based on augmentation layers, 
-    and the possibility to insert dropout layers. Optionally, the trained model can be saved.
+    Trains the given Keras model with options for dynamic adjustments, augmentation layer management, and dropout insertion.
+    Optionally saves the trained model.
 
     Args:
         model (tf.keras.Model): The Keras model to be trained.
-        X_train (tuple): Tuple containing the training data and labels. Format: (training_data, labels).
-        valid_set (tuple): Tuple containing the validation data and labels. Format: (validation_data, labels).
+        X_train (tuple): Tuple containing training data and labels. Format: (training_data, labels).
+        valid_set (tuple): Tuple containing validation data and labels. Format: (validation_data, labels).
         epochs (int): Number of epochs to train the model.
-        optimiser (tf.keras.optimizers.Optimizer): Optimizer to be used for training the model.
-        loss (str or tf.keras.losses.Loss): Loss function to be used for training.
-        callbacks (dict, optional): Dictionary of Keras callbacks. Keys should be callback class names, and values should be callback instances.
-        with_aug_layer (bool, optional): Whether to adjust model based on the presence of an augmentation layer. Defaults to True.
+        params (dict, optional): Parameters for model compilation, e.g., {'optimizer': optimizer, 'loss': loss_function}. Defaults to None.
+        callbacks (dict, optional): Dictionary of Keras callbacks. Keys should be callback class names, values should be callback instances. Defaults to None.
+        with_aug_layer (bool, optional): Whether to include the augmentation layer in the model. Defaults to True.
         aug_layer_name (str, optional): Name of the augmentation layer to identify and remove if `with_aug_layer` is False. Defaults to None.
-        insert_dropout (tf.keras.layers.Layer, optional): Dropout layer to be inserted before the final layer if augmentation is removed. Defaults to None.
-        save_model_to (str, optional): Path to save the entire model after training. Defaults to None.
-        show_model_summary: whether to show the model summary
-        kwargs: passed to model fit method.
+        insert_dropout (tf.keras.layers.Layer, optional): Dropout layer to be inserted before the final layer if the augmentation layer is removed. Defaults to None.
+        save_model_to (str, optional): Path to save the model after training. Defaults to None.
+        show_model_summary (bool, optional): Whether to print the model summary. Defaults to False.
+        **kwargs: Additional arguments passed to the `model.fit` method.
 
     Returns:
         tf.keras.callbacks.History: The training history object.
@@ -38,24 +37,28 @@ def train_model(model,
     if callbacks is None:
         callbacks = {}
 
-    # Display with_aug_layer state
+    # Display augmentation layer status
     print(f'Training with with_aug_layer={with_aug_layer}')
     
+    # Adjust model if not using augmentation layer
     if not with_aug_layer:
         if model.layers[0].name == 'augmentation_layer' or model.layers[0].name == aug_layer_name:
             layers = model.layers[1:]
             if insert_dropout:
-                index = layers.index(layers[-1])
+                # Insert dropout layer before the final layer
+                index = len(layers) - 1
                 layers.insert(index, insert_dropout)
             model = tf.keras.Sequential(layers=layers, name='final_gender_clf_model')
             
     if show_model_summary:
-        print(model.summary())
+        # Print model summary
+        model.summary()
 
-    model.compile(
-        **params
-    )
+    if params:
+        # Compile the model with provided parameters
+        model.compile(**params)
 
+    # Train the model
     history = model.fit(
         X_train,
         epochs=epochs,
